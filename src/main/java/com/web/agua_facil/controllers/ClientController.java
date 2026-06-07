c2e88d04e3acd27188eb57d06fba3f133a1b86d1
apccarvalho <andreiacarvalho.apc@gmail.com>
2026-06-07 14:29:18 -0300

    Criação do ClientController

---
+++ src/main/java/com/web/agua_facil/controllers/ClientController.java
@@ -0,0 +1,62 @@
+package com.web.agua_facil.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import com.web.agua_facil.models.Client;
+import com.web.agua_facil.services.ClientService;
+import jakarta.validation.Valid;
+
+@Controller
+public class ClientController {
+
+    private final ClientService clientService;
+
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @GetMapping("/client")
+    public String index(Model model) {
+        model.addAttribute("clientsList", clientService.getAllClients());
+        return "client/index";
+    }
+
+    @GetMapping("/client/create")
+    public String create(Model model) {
+        model.addAttribute("client", new Client());
+        return "client/create";
+    }
+
+    @PostMapping("/client/save")
+    public String save(@Valid @ModelAttribute("client") Client client, BindingResult result) {
+        if (result.hasErrors()) {
+            return "client/create";
+        }
+        clientService.saveClient(client);
+        return "redirect:/client";
+    }
+
+    @GetMapping("/client/edit/{id}")
+    public String showEditForm(@PathVariable Long id, Model model) {
+        model.addAttribute("client", clientService.getClientById(id));
+        return "client/edit";
+    }
+
+    @PostMapping("/client/edit/{id}")
+    public String edit(@PathVariable Long id, @Valid @ModelAttribute("client") Client client, BindingResult result) {
+        if (result.hasErrors()) {
+            return "client/edit";
+        }
+        client.setId(id);
+        clientService.saveClient(client);
+        return "redirect:/client";
+    }
+
+    @PostMapping("/client/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        clientService.deleteClientById(id);
+        return "redirect:/client";
+    }
+}
