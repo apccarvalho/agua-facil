cccf029a821f46e2c1ea05632db26357d36d106e
apccarvalho <andreiacarvalho.apc@gmail.com>
2026-06-07 12:29:10 -0300

    Create - criação do projeto

---
+++ src/test/java/com/web/agua_facil/AguaFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.agua_facil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AguaFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
